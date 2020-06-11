f3070775acb475fe424822aa928a69cd826b5a0b
Crowdbotics <team@crowdbotics.com>
2020-06-11 07:26:44 +0000

    Update models

--- web_11_dev_5846/wsgi.py
+++ web_11_dev_5846/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web_11_dev_5846.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web_11_dev_5846.settings")
 
 application = get_wsgi_application()
